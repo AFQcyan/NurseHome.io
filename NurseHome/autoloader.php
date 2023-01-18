a12c2ee7d53cfe4e938d1f19ef61da2d6d384054
AFQcyan <91124299+AFQcyan@users.noreply.github.com>
2023-01-18 13:16:50 +0900

    commit

--- NurseHome/autoloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function autoloader($c) {
-    require __ROOT . __DS . str_replace('\\', __DS, $c) . '.php';
-}
-
-spl_autoload_register('autoloader');
--- NurseHome/src/View/layout/footer.php
+++
@@ -1,4 +0,0 @@
-<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="./../resources/js/index.js"></script>
